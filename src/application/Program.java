7a068b43a34e73f1a780f900dcfd366e956f917d
Luciano Holanda Porto <lug.dba@gmail.com>
2021-03-17 22:28:40 -0300

    2ª versão - solução com bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,14 +9,8 @@ public class Program {
     public static void main(String[] args) {
 
         String path = "c:\\temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-            // br = new BufferedReader(new FileReader(path));
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -26,19 +20,6 @@ public class Program {
         }
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
